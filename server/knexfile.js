1603799be624be686305c33dd46cf9a4d9cf2e7f
Marko Martonosi <m.martonosi@vegaitsourcing.rs>
2019-03-22 17:14:10 +0100

    updated

---
+++ server/knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      // filename: 'postgresql_local/timesheet',
+      user : 'postgres',
+      password : 'password',
+      database: 'timesheet',
+    }
+  },
+};
